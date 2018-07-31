b2ce65a74dc1ff364dacb9754699f67a94559c93
Christopher A. Williamson <home@chrisaw.com>
2018-07-31 10:28:28 +0100

    added note to composer install script to explain weird filenames

--- deploy_scripts/1-install-composer.sh
+++ deploy_scripts/1-install-composer.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
 
+#
+# NOTE:
+#
+# Composer files start with filenames including 'skipauto' in order to skip
+# the automation enabled by the site extension "Composer". Unfortunately this
+# site extension is force-enabled in our sandpit environments and cannot be
+# configured to use PHP 7.1. This breaks Magento composer deployments so this
+# workaround skips that stage entirely - allowing us to deploy them using this
+# script which *does* use the correct PHP version.
+#
+
 # fetch and run installer
 curl -s https://getcomposer.org/installer | php
 
 # change to web dir
 cd ../wwwroot
 
-# move composer files in to place (used to skip azure site extension - breaks install)
+# move composer files in to place
 mv composer.skipauto.json composer.json
 mv composer.skipauto.lock composer.lock
 
