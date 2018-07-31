a010cb7a7f824b82c3ba67819a092aa8940c03cc
Christopher A. Williamson <home@chrisaw.com>
2018-07-31 10:21:24 +0100

    sorted vars

--- setup_vars.sh
+++ setup_vars.sh
@@ -2,14 +2,14 @@
 
 # set these parameters before doing anything else!
 APP_EMAIL=chris.williamson@ecs.co.uk
-APP_SKU=S1
 APP_FIRSTNAME=Christopher
 APP_LASTNAME=Williamson
-APP_USER=admin
 APP_PASS=Pa55W0rd!
-DB_USER=app
+APP_SKU=S1
+APP_USER=admin
 DB_PASS=Pa55W0rd!
 DB_SKU=B_Gen5_1
+DB_USER=app
 GIT_REPO=https://github.com/cawilliamson/azure-magento.git # HTTPS Repo URL
 LOCATION=uksouth # azure location
 NAME=magentoecs # site name
