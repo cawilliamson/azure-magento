5afe05bed74fa2ef5bd49613298c17ede91b7024
Christopher A. Williamson <home@chrisaw.com>
2018-07-31 10:13:59 +0100

    added workaround for site extension (composer install)

--- deploy_scripts/1-install-composer.sh
+++ deploy_scripts/1-install-composer.sh
@@ -6,6 +6,9 @@ curl -s https://getcomposer.org/installer | php
 # change to web dir
 cd ../wwwroot
 
+# move composer.json in to place (used to skip azure site extension - breaks install)
+mv composer.skipauto.json composer.json
+
 # install/update composer modules
 if [ ! -f ./vendor/autoload.php ]; then
   php ../repository/composer.phar install
