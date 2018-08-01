4239f51b2ed1b2caf812bc07b3d52443b1640c48
Christopher A. Williamson <home@chrisaw.com>
2018-08-01 06:38:16 +0100

    added failsafe for env.php issues

--- deploy_scripts/2-magento.cmd
+++ deploy_scripts/2-magento.cmd
@@ -13,6 +13,9 @@ CD ..\wwwroot
 
 REM run install steps
 IF NOT EXIST app\etc\local.xml (
+  REM reset env file
+  DEL app\etc\env.php
+  
   REM run setup wizard
   php bin\magento setup:install --admin-firstname="%APP_FIRSTNAME%" --admin-lastname="%APP_LASTNAME%" --admin-email="%APP_EMAIL%" --admin-user="%APP_USER%" --admin-password="%APP_PASS%" --db-host="%NAME%sql.mysql.database.azure.com" --db-name="%NAME%db" --db-user="%DB_USER%@%NAME%sql" --db-password="%DB_PASS%" --no-interaction
 
