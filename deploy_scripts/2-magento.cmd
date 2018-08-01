a780166d0631efc1c465b8075a913fa63a5967d7
Christopher A. Williamson <home@chrisaw.com>
2018-08-01 10:42:54 +0100

    removed useless step from deployment

--- deploy_scripts/2-magento.cmd
+++ deploy_scripts/2-magento.cmd
@@ -15,7 +15,4 @@ REM run install steps
 IF NOT EXIST app\etc\local.xml (
   REM run setup wizard
   php bin\magento setup:install --admin-firstname="%APP_FIRSTNAME%" --admin-lastname="%APP_LASTNAME%" --admin-email="%APP_EMAIL%" --admin-user="%APP_USER%" --admin-password="%APP_PASS%" --db-host="%NAME%sql.mysql.database.azure.com" --db-name="%NAME%db" --db-user="%DB_USER%@%NAME%sql" --db-password="%DB_PASS%" --no-interaction
-
-  REM install sample data
-  php bin\magento sampledata:deploy %APP_USER%
 )
